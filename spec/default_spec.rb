5bc9b34813d4f5c84d3474c57b0f8d3b8db56a3c
wate <pc.poisoning@gmail.com>
2017-12-17 20:44:54 +0900

    Debian用に書き直し

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,28 +1,39 @@
 require 'spec_helper'
 
-describe 'role common' do
-  property['common_packages'].each do |pkg|
-    describe package(pkg) do
-      it { should be_installed }
-    end
+property['common_packages'].each do |pkg|
+  describe package(pkg) do
+    it { should be_installed }
   end
+end
+property['common_users'].each do |user|
+  describe user(user['name']) do
+    it { should exist }
+    it { should belong_to_group 'adm' } if user.key?('admin') && user['admin']
+  end
+end
 
-  describe package('firewalld') do
-    it { should be_installed }
-  end
-  describe package('etckeeper') do
-    it { should be_installed }
-  end
-  describe package('fail2ban') do
-    it { should be_installed }
-  end
+describe package('ufw') do
+  it { should be_installed }
+end
 
-  describe service('firewalld') do
-    it { should be_enabled }
-    it { should be_running }
-  end
-  describe service('fail2ban') do
-    it { should be_enabled }
-    it { should be_running }
-  end
+describe package('fail2ban') do
+  it { should be_installed }
+end
+
+describe file('/etc/fail2ban/jail.local') do
+  it { should exist }
+  it { should be_file }
+end
+
+describe package('etckeeper') do
+  it { should be_installed }
+end
+
+describe service('ufw') do
+  it { should be_enabled }
+  it { should be_running }
+end
+describe service('fail2ban') do
+  it { should be_enabled }
+  it { should be_running }
 end
