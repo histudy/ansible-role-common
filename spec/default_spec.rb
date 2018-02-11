a9039e608206324b3e3cbfc0cd95cef39cf1dfe5
wate <pc.poisoning@gmail.com>
2018-02-11 20:41:49 +0900

    各処理を分離

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,9 +1,28 @@
 require 'spec_helper'
 
+property['common_groups'].each do |group|
+  describe group(group['name']) do
+    if group.key?('remove') && group['remove']
+      it { should_not exist }
+    else
+      it { should exist }
+    end
+  end
+end
+
 property['common_users'].each do |user|
   describe user(user['name']) do
-    it { should exist }
-    it { should belong_to_group 'adm' } if user.key?('admin') && user['admin']
+    if user.key?('remove') && user['remove']
+      it { should_not exist }
+    else
+      it { should exist }
+      it { should belong_to_group 'adm' } if user.key?('admin') && user['admin']
+      if user.key?('groups')
+        user['groups'].each do |group|
+          it { should belong_to_group group }
+        end
+      end
+    end
   end
 end
 
@@ -16,6 +35,7 @@ end
 describe package('ufw') do
   it { should be_installed }
 end
+
 describe package('etckeeper') do
   it { should be_installed }
 end
@@ -24,7 +44,7 @@ describe package('fail2ban') do
   it { should be_installed }
 end
 
-describe file('/etc/fail2ban/jail.local') do
+describe file('/etc/fail2ban/jail.d/local.conf') do
   it { should exist }
   it { should be_file }
 end
